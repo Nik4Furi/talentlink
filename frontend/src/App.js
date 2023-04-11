d02d914e567021aa834fbc4c31c72d6dfbe30304
pankaj7073 <pankajvaishnav128@gmail.com>
2023-04-11 14:54:22 +0530

    Login Page Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import react from 'react'
-
+import Login from './pages/login/Login';
 const App = () => {
   return (
     <>
-      <h1>Hello Achievers</h1>
+      {/* <h1>Hello Achievers</h1> */}
+      <Login />
     </>
   )
 }
